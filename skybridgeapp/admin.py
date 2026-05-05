eba6107c0f870bbe71558e69222037d4053a0578
Guilhermefrv <romaoguilherme557@gmail.com>
2026-05-05 20:26:57 -0300

    chore(admin): registra models no Django admin

--- skybridgeapp/admin.py
+++ skybridgeapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Passageiro, Funcionario, Administrador, Voo, Aeronave, Reserva, Bilhete, Bagagem, CheckIn, PortaoEmbarque, Notificacao
 
-# Register your models here.
+
+admin.site.register(Passageiro)
+admin.site.register(Funcionario)
+admin.site.register(Administrador)
+admin.site.register(Voo)
+admin.site.register(Aeronave)
+admin.site.register(Reserva)
+admin.site.register(Bilhete)
+admin.site.register(Bagagem)
+admin.site.register(CheckIn)
+admin.site.register(PortaoEmbarque)
+admin.site.register(Notificacao)
